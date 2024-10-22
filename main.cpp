490ce309c43ddb1ceb32e75876c9c4093859bbbd
Dmitry <takesxq77@gmail.com>
2024-10-22 13:14:18 +0300

    LOL

--- main.cpp
+++ main.cpp
@@ -1,5 +1,29 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Document {
+    int id;
+    int rating;
+};
+
+void PrintDocuments(vector<Document> documents, size_t skip_start, size_t skip_finish) {
+    if (documents.empty() || skip_start >= documents.size() || skip_finish >= documents.size()) {
+    return;
+} 
+    std::sort(documents.begin(),documents.end(),[](const Document& r_a , const Document& r_b){
+      
+        return r_a.rating > r_b.rating;
+    });
+  
+ for (size_t i = skip_start; i < documents.size() - skip_finish; ++i) {
+        cout << "{" << " id = " << documents[i].id << " rating = " << documents[i].rating << "}" << endl;
+    
+    }
+}
 
 int main() {
-    std::cout << "Hello, Git!" << std::endl;
+    PrintDocuments({{100, 5}, {101, 7}, {102, -4}, {103, 9}, {104, 1}}, 1, 2);
 }
